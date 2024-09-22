d1efa3a34e35709bfd0b102ae758f5715a3a0a00
Victoria-2 <114245919+Victoria-2@users.noreply.github.com>
2024-09-22 19:15:46 -0300

    cambios para ejecucion

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -5,9 +5,9 @@ const getPokemonAllQuery = (req = request, res = response) => {
   const { offset = '', limit = '' } = req.query
   console.log(offset, limit)
 
-  if (isNaN(Number(offset)) || isNaN(Number(limit)) || Number(offset) < 0 || Number(limit) < 1 || !(Number.isInteger(offset)) || !(Number.isInteger(limit))) {
+  /* if (isNaN(Number(offset)) || isNaN(Number(limit)) || Number(offset) < 1 || Number(limit) < 1 || !(Number.isInteger(offset)) || !(Number.isInteger(limit))) {
     return res.status(400).json({ msg: 'Debe ingresar numeros validos para los limites!' })
-  }
+  } */
 
   const filtro1 = (offset) ? `?offset=${offset}` : '?offset=0'
   const filtro2 = (limit) ? `&limit=${limit}` : '&limit=50' // muestra 50 pokemon
