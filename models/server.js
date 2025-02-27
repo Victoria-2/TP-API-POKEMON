65e057a9fee6d8d51f23ce319d80eac078e258f7
Victoria-2 <114245919+Victoria-2@users.noreply.github.com>
2025-02-27 10:32:38 -0300

    Se agregó CORS
    Cuando se llama a la app en vez desde un emulador android en la web,  tengo este error:
    ClientException: XMLHttpRequest error.
    --> Solucion

--- models/server.js
+++ models/server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 
 class Server {
   constructor () {
@@ -9,6 +10,7 @@ class Server {
   }
 
   middleware () {
+    this.app.use(cors())
     this.app.use(express.static('public'))
   }
 
