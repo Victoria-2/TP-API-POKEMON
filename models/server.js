de5cec5d74e77308a0a5c3bd25d6516918d6bf71
Victoria-2 <114245919+Victoria-2@users.noreply.github.com>
2025-02-27 10:27:47 -0300

    Update server.js

--- models/server.js
+++ models/server.js
@@ -3,7 +3,7 @@ const express = require('express')
 class Server {
   constructor () {
     this.app = express()
-    // this.port = process.env.PORT || 3000
+    this.port = process.env.PORT || 3000
     this.middleware()
     this.rutas()
   }
